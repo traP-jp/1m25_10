2a3eb9bd5162b8d9252140955d02415233480b50
otukado <otukado@gmail.com>
2025-08-20 17:37:00 +0900

    feat: create SidebarItem component

---
+++ frontend/src/components/sideBar/SidebarItem.vue
@@ -0,0 +1,62 @@
+<template>
+    <router-link
+        :to="path"
+        :class="[$style.sidebarItem, { [$style['is-active']]: isActive }]"
+    >
+        <img :src="icon"/>
+        <span :class="$style.title">
+            {{ title }}
+        </span>
+    </router-link>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const props = defineProps<{
+    icon: string
+    title: string
+    path: string
+}>()
+
+const route = useRoute()
+const isActive = computed(() => route.path === props.path)
+</script>
+
+<style lang="scss" module>
+.sidebarItem {
+    display: flex;
+    padding: 8px 16px;
+    align-items: center;
+    gap: 16px;
+    color: #333;
+    border-radius: 32px;
+    transition: background-color 0.3s;
+    text-decoration: none;
+}
+
+.sidebarItem:hover {
+  background-color: #E2E5E9;
+  /*色はあとでやる*/
+}
+
+.title {
+    color: var(--Light-UI-Primary, #49535B);
+    font-family: Inter;
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+
+.sidebarItem.is-active {
+  background-color: #D8E0EB;
+}
+
+.sidebarItem.is-active .title {
+  color: #005BAC;
+  font-weight: bold;
+}
+</style>
