e826f376e1131a61b173aa508f4b71548c1e397c
quaran <skrsoma37@gmail.com>
2025-09-06 12:48:47 +0900

    refactor: remove unused User related implemantation

--- backend/pkg/database/migrations/1_schema.sql
+++ backend/pkg/database/migrations/1_schema.sql
@@ -1,7 +1,3 @@
 -- +goose Up
-CREATE TABLE IF NOT EXISTS users (
-	id VARCHAR(36) NOT NULL,
-	name VARCHAR(255) NOT NULL,
-	email VARCHAR(255) NOT NULL,
-	PRIMARY KEY (id)
-);
+-- Users table removed as authentication is handled via X-Forwarded-User header
+-- User information will be retrieved directly from traQ API when needed
