c4d16b8b8218b8eed6d175f241bb07b354898f1d
mikannkann <82451989+mikannkann@users.noreply.github.com>
2025-09-09 09:17:31 +0900

    chore: change fallback stamp ID for album chance

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -4,4 +4,4 @@
 // 通常は環境変数 VITE_ALBUM_CHANCE_STAMP_ID から取得しますが、
 // 未設定の場合に限りこの値を使用します。
 // 参考: テストビューでも例示されているUUID
-export const ALBUM_CHANCE_STAMP_ID = 'bd6cdc20-1ee8-4072-a37a-1e48f1e549c0'
+export const ALBUM_CHANCE_STAMP_ID = '019922d4-2449-766e-ad16-b83f2622f5eb'
